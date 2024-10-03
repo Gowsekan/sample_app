3483d28bcb45c5f82de7f86c514b60267c2c5456
“Gowsekan” <gowsekana@gmail.com>
2024-10-03 17:15:48 +0530

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,6 +48,7 @@ class UserTest < ActiveSupport::TestCase
   end
   test "email addresses should be unique" do
     duplicate_user = @user.dup
+    #uplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
@@ -69,4 +70,33 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
-end
+
+    test "should follow and unfollow a user" do
+      michael = users(:michael)
+      archer = users(:archer)
+      assert_not michael.following?(archer)
+      michael.follow(archer)
+      assert michael.following?(archer)
+      assert archer.followers.include?(michael)
+      michael.unfollow(archer)
+      assert_not michael.following?(archer)
+    end
+
+      test "feed should have the right posts" do
+        michael = users(:michael)
+        archer  = users(:archer)
+        lana    = users(:lana)
+        # Posts from followed user
+        lana.microposts.each do |post_following|
+          assert michael.feed.include?(post_following)
+        end
+        # Posts from self
+        michael.microposts.each do |post_self|
+          assert michael.feed.include?(post_self)
+        end
+        # Posts from unfollowed user
+        archer.microposts.each do |post_unfollowed|
+          assert_not michael.feed.include?(post_unfollowed)
+        end
+      end
+    end
